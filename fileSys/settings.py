dde8a157c5cadbe9bcb6520bc6947ed6bd15d899
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2024-05-22 09:40:46 +0000

    updated backend settings

--- fileSys/settings.py
+++ fileSys/settings.py
@@ -128,3 +128,19 @@ STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'fileApp.MyUser'
+
+DEFAULT_FROM_EMAIL = CONFIGS['DEFAULT_EMAIL']
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = CONFIGS['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = CONFIGS['EMAIL_HOST_PASSWORD']
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
+
--- fileSys/urls.py
+++ fileSys/urls.py
@@ -18,13 +18,22 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic.base import TemplateView
 
 from fileApp import views as fileview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__reload__/', include('django_browser_reload.urls')),
-    path('registration/', fileview.Registration.as_view(), name='registration' )
+    path('password_reset/', fileview.password_reset_request, name='password_reset'),
+    path('reset/<uidb64>/<token>/', fileview.password_reset_confirm, name='password_reset_confirm'),
+    path('password_reset/done/', TemplateView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/done/', TemplateView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
+    path('signup/', fileview.SignupView.as_view(), name='signup'),
+    path('login/', fileview.LoginView.as_view(), name='login'),
+    path('files/', fileview.FileFeed.as_view(), name="files"),
+    path('share_file/', fileview.share_file, name='share_file'),
+    path('download_file/', fileview.download_file, name='download_file'),
 ]
 
 urlpatterns += static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
